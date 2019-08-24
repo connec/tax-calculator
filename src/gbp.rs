d3321df418192950bc8822c7d75a153544350254
Chris Connelly <chris@connec.co.uk>
2019-08-24 21:04:14 +0100

    fix: Improve GBP parsing and formatting
    The num-format crate is used for formatting thousand separators and
    Rust's built-in formatting can format the decimals, so there's no need
    for the hokey custom stuff.
    
    Parsing is still a bit nasty, but will now work with valid decimal
    values.

--- src/gbp.rs
+++ src/gbp.rs
@@ -1,53 +1,94 @@
-use itertools::Itertools;
+//! Defines a struct that wraps a GBP amount.
+
+use num_format::{Locale, ToFormattedString};
 use std::fmt;
 use std::num::ParseIntError;
 
+/// The maximum amount of GBP that can be represented.
 pub static MAX: Gbp = Gbp(std::u32::MAX);
 
-/// Represents an amount of Great British Pounds.
+/// Represents errors that can occur when parsing GBP.
 ///
-/// This provides useful formatting and parsing methods and implements arithemtic operators for
-/// convenience.
+/// This is a superset of the errors that can occur when parsing an integer with one additional
+/// case: the decimal value may be invalid (too long or short).
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidFloat(ParseIntError),
+    InvalidDecimal,
+}
+
+impl From<ParseIntError> for ParseError {
+    fn from(error: ParseIntError) -> Self {
+        ParseError::InvalidFloat(error)
+    }
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ParseError::InvalidFloat(error) => error.fmt(f),
+            ParseError::InvalidDecimal => write!(f, "Invalid decimal for GBP value"),
+        }
+    }
+}
+
+/// Represents an amount of Great British Pounds, stored as whole pence.
+///
+/// This provides useful formatting and parsing methods and implements necssary arithemtic operators
+/// for convenience.
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Gbp(u32);
 
 impl Gbp {
+    /// Construct a `Gbp` from a number of pounds.
+    ///
+    /// This accounts for the fact that we store values as pence.
     pub fn from_pounds(pounds: u32) -> Self {
         Gbp(pounds * 100)
     }
 }
 
 impl fmt::Display for Gbp {
+    /// Display a GBP amount, formatted like: £1,234,567.89
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let string = self.0.to_string();
-        let (pounds, pence) = if string.len() > 2 {
-            string.split_at(string.len() - 2)
-        } else {
-            ("", string.as_str())
-        };
-        let pounds = pounds
-            .as_bytes()
-            .rchunks(3)
-            .rev()
-            .intersperse(&[b','])
-            .flatten()
-            .map(|c| char::from(*c))
-            .collect::<String>();
-        write!(f, "£{}.{}", pounds, pence)
+        let pounds = self.0 / 100;
+        let pence = self.0 % 100;
+        write!(
+            f,
+            "£{}.{:02}",
+            pounds.to_formatted_string(&Locale::en),
+            pence
+        )
     }
 }
 
 impl std::str::FromStr for Gbp {
-    type Err = ParseIntError;
+    type Err = ParseError;
 
-    /// This is a slightly naive approach but it keeps things simple: we strip leading "£", and any
-    /// ",", then parse it as a u32.
-    fn from_str(mut string: &str) -> Result<Self, ParseIntError> {
-        if string.starts_with("£") {
-            string = &string["£".len()..];
+    /// Parse a currency amount from a string.
+    ///
+    /// This is a slightly naive approach but it keeps things simple: we strip leading "£", split
+    /// pounds and pence, remove any "," from the pounds, then combine and parse as a u32.
+    fn from_str(mut string: &str) -> Result<Self, ParseError> {
+        string = string.trim_start_matches('£');
+        let (pounds, pence) = match string.find('.') {
+            Some(index) => {
+                let split = string.split_at(index);
+                (split.0, split.1.trim_start_matches('.'))
+            }
+            None => (string, "00"),
+        };
+        if pence.len() != 2 {
+            return Err(ParseError::InvalidDecimal);
         }
-        let string = string.chars().filter(|c| *c != ',').collect::<String>();
-        Ok(Gbp(100 * string.parse::<u32>()?))
+        let pounds = pounds
+            .chars()
+            .filter(|c| *c != ',')
+            .collect::<String>()
+            .parse::<u32>()?
+            * 100;
+        let pence = pence.parse::<u32>()?;
+        Ok(Gbp(pounds + pence))
     }
 }
 
